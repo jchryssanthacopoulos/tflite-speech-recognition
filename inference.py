078a761dd6efab5d7c4125213d7ea820feeabdbd
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2021-01-01 17:54:06 -0500

    Add inference script

--- inference.py
+++ inference.py
@@ -1,11 +1,25 @@
 """Run inference on directory of sound files."""
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
+import glob
 import sys
 
 import librosa
+import numpy as np
+import python_speech_features
+from tensorflow.keras import layers, models
 
 
-def signal_to_mfcc(signal, fs):
+sample_rate = 8000
+num_mfcc = 16
+
+
+def calc_mfcc(path):
+    # Load wavefile
+    signal, fs = librosa.load(path, sr=sample_rate)
+
     # Create MFCCs from sound clip
     mfccs = python_speech_features.base.mfcc(signal, 
                                             samplerate=fs,
@@ -21,15 +35,21 @@ def signal_to_mfcc(signal, fs):
     return mfccs.transpose()
 
 
-def calc_mfcc(path):
-    # Load wavefile
-    signal, fs = librosa.load(path, sr=sample_rate)
-    return signal_to_mfcc(signal, fs)
 
-
-
+def get_model_score(model, sound_file):
+    mfccs = calc_mfcc(sound_file)
+    mfccs = mfccs.reshape((1, num_mfcc, num_mfcc, 1))
+    return model.predict(mfccs)[0][0]
 
 
 
 if __name__ == '__main__':
-    directory = sys.argv[1]
+    # Usage is: python inference.py <model_path> <sound_files_path>
+    model_name, directory = sys.argv[1], sys.argv[2]
+
+    model = models.load_model(model_name)
+    sound_files = glob.glob(f"{directory}/*.wav")
+
+    for sound_file in sound_files:
+        score = get_model_score(model, sound_file)
+        print(f"Score on file {sound_file}: {score}")
